9d1f226374d4979c596149ec253c894af0372547
AMIT SINGH <amitsingh@AMITs-MacBook-Air.local>
2024-07-21 16:56:47 +0530

    Navbar changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,12 +19,6 @@ function Navbar() {
         <Link to="/home" className="text-2xl font-semibold">
           Medium
         </Link>
-        <button
-          className="md:hidden text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={handleLogOut}
-        >
-          Log Out
-        </button>
       </div>
       <div className="flex flex-col md:flex-row items-center mt-2 md:mt-0">
         {pageData.map((page, index) => (
